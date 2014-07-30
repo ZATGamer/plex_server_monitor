2c220b07f4a063a38cf1d4b693bea683bbd7da7d
Anthony Barragree <zatgaming@gmail.com>
2014-07-30 15:37:05 -0600

    SB Monitor test

--- sickbeard_monitor.py
+++ sickbeard_monitor.py
@@ -5,6 +5,7 @@ import json
 from gpiocrust import Header, OutputPin
 import time
 import socket
+import datetime
 
 
 def call_sb(sb_api_key, sb_ip, sb_port, sb_api):
@@ -21,31 +22,34 @@ def call_sb(sb_api_key, sb_ip, sb_port, sb_api):
 
 
 if __name__ == '__main__':
+    last_run = datetime.datetime.now()
+    sb_api_key = 'd1adef047c3d9c52bf65bfe709831b18'
+    sb_ip = '10.180.181.120'
+    sb_port = '8081'
+    sb_api = 'sb.ping'
+
     while True:
-        sb_api_key = 'd1adef047c3d9c52bf65bfe709831b18'
-        sb_ip = '10.180.181.120'
-        sb_port = '8081'
-        sb_api = 'sb.ping'
+        if last_run + datetime.timedelta(seconds=10) >= datetime.datetime.now():
 
-        response = call_sb(sb_api_key, sb_ip, sb_port, sb_api)
+            response = call_sb(sb_api_key, sb_ip, sb_port, sb_api)
 
-        try:
-            response2 = json.load(response)
+            try:
+                response2 = json.load(response)
 
-        except:
-            response2 = {'result': 'broke'}
+            except:
+                response2 = {'result': 'broke'}
 
-        test = response2['result']
+            test = response2['result']
 
-        if test == 'success':
-            print 'Sickbeard is up'
-            with Header() as header:
-                pin = OutputPin(22, value=1)
-        else:
-            print 'Sickbeard is down'
-            with Header() as header:
-                pin = OutputPin(22, value=0)
-        time.sleep(30)
+            if test == 'success':
+                print 'Sickbeard is up'
+                with Header() as header:
+                    pin = OutputPin(22, value=1)
+            else:
+                print 'Sickbeard is down'
+                with Header() as header:
+                    pin = OutputPin(22, value=0)
+
 
 
 
