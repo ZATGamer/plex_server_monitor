48660d80e1d9784e7ea2ee9167975a34c546fa83
Anthony Barragree <zatgaming@gmail.com>
2014-07-31 13:30:20 -0600

    SB Monitor test

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.OUT)
-#GPIO.setup(13, GPIO.OUT)
-#GPIO.setup(15, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 red = GPIO.PWM(11, 50)
-
+green = GPIO.PWM(13, 50)
+blue = GPIO.PWM(15, 50)
 red.start(0)
 while True:
-        red.ChangeDutyCycle(50)
-
+    red.ChangeDutyCycle(50)
+    green.ChangeDutyCycle(50)
+    blue.ChangeDutyCycle(50)
