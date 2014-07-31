455618a5119e63d394f32ce51821920de11613db
Anthony Barragree <zatgaming@gmail.com>
2014-07-31 13:24:43 -0600

    SB Monitor test

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+#GPIO.setup(13, GPIO.OUT)
+#GPIO.setup(15, GPIO.OUT)
+
 red = GPIO.PWM(7, 50)
 
 red.start(0)
